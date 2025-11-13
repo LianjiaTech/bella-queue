50a2700c1a7e0a7c4194964a56ded8016289666d
jiand.yuan001 <jiand.yuan001@ke.com>
2025-11-13 21:25:05 +0800

    fixbug: 解决队列中数据没过期，但是元数据已经过期的问题

--- api/src/main/resources/lua/online/enqueue.lua
+++ api/src/main/resources/lua/online/enqueue.lua
@@ -6,7 +6,7 @@ local capacity = tonumber(ARGV[5])
 
 -- 存储任务元数据和添加到队列
 local taskKey = queueName .. ":metadata:" .. taskId
-redis.call('SETEX', taskKey, 60 * 10, taskMetadata)
+redis.call('SETEX', taskKey, 60 * 60 * 24, taskMetadata)
 redis.call('ZADD', queueName, taskStartTime, taskId)
 
 -- 设置队列过期时间
